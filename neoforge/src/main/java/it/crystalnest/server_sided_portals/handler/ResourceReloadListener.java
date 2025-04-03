255ab36ccd99f61df6c5f5e9541f63c196021391
Crystal Spider <riccardo.chimisso@gmail.com>
2025-04-03 17:21:18 +0200

    Fix feature backporting.

--- neoforge/src/main/java/it/crystalnest/server_sided_portals/handler/ResourceReloadListener.java
+++ neoforge/src/main/java/it/crystalnest/server_sided_portals/handler/ResourceReloadListener.java
@@ -3,7 +3,7 @@ package it.crystalnest.server_sided_portals.handler;
 import it.crystalnest.server_sided_portals.Constants;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
 
 /**
  * Handles datapack reload events.
@@ -11,12 +11,12 @@ import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 @EventBusSubscriber(modid = Constants.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public final class ResourceReloadListener extends CommonResourceReloadListener {
   /**
-   * Handles the {@link AddServerReloadListenersEvent}.
+   * Handles the {@link AddReloadListenerEvent}.
    *
-   * @param event {@link AddServerReloadListenersEvent}.
+   * @param event {@link AddReloadListenerEvent}.
    */
   @SubscribeEvent
-  public static void handle(AddServerReloadListenersEvent event) {
-    event.addListener(Constants.DIMENSION_TWEAKS_ID, new ResourceReloadListener());
+  public static void handle(AddReloadListenerEvent event) {
+    event.addListener(new ResourceReloadListener());
   }
 }
