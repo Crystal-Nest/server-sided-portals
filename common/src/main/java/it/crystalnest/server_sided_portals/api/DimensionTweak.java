91ae5c64ebb0e11914fe7ee82ef28e124c0c0d27
Crystal Spider <riccardo.chimisso@gmail.com>
2025-04-06 15:18:23 +0200

    Fix feature backporting for 1.19.2.

--- common/src/main/java/it/crystalnest/server_sided_portals/api/DimensionTweak.java
+++ common/src/main/java/it/crystalnest/server_sided_portals/api/DimensionTweak.java
@@ -1,8 +1,18 @@
 package it.crystalnest.server_sided_portals.api;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 
+import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,12 +28,18 @@ public record DimensionTweak(ResourceKey<Level> dimension, ResourceKey<Level> co
    */
   public static final DimensionTweak OVERWORLD_CONNECTION = new DimensionTweak(null, Level.OVERWORLD, List.of());
 
-  public DimensionTweak {
-    if (connection == null) {
-      connection = Level.OVERWORLD;
-    }
-    if (gamemode == null) {
-      gamemode = List.of();
+  /**
+   * {@link DimensionTweak} {@link JsonDeserializer}.
+   */
+  public static class Deserializer implements JsonDeserializer<DimensionTweak> {
+    @Override
+    public DimensionTweak deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+      JsonObject object = json.getAsJsonObject();
+      return new DimensionTweak(
+        ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(object.get("dimension").getAsString())),
+        object.has("connection") ? ResourceKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(object.get("connection").getAsString())) : Level.OVERWORLD,
+        object.has("gamemode") ? context.deserialize(object.get("gamemode"), new TypeToken<List<GamemodeTweak>>() {}.getType()) : Collections.emptyList()
+      );
     }
   }
 }
--- common/src/main/java/it/crystalnest/server_sided_portals/handler/CommonResourceReloadListener.java
+++ common/src/main/java/it/crystalnest/server_sided_portals/handler/CommonResourceReloadListener.java
@@ -2,22 +2,17 @@ package it.crystalnest.server_sided_portals.handler;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 import it.crystalnest.server_sided_portals.Constants;
 import it.crystalnest.server_sided_portals.api.DimensionTweak;
 import it.crystalnest.server_sided_portals.api.GamemodeTweak;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
@@ -41,7 +36,7 @@ public class CommonResourceReloadListener extends SimpleJsonResourceReloadListen
   protected CommonResourceReloadListener() {
     super(new Gson(), Constants.DIMENSION_TWEAKS_ID.getPath());
     gson = new GsonBuilder()
-      .registerTypeAdapter(new TypeToken<ResourceKey<Level>>() {}.getType(), new ResourceKeyDeserializer())
+      .registerTypeAdapter(DimensionTweak.class, new DimensionTweak.Deserializer())
       .registerTypeAdapter(GamemodeTweak.class, new GamemodeTweak.Deserializer())
       .create();
   }
@@ -58,16 +53,6 @@ public class CommonResourceReloadListener extends SimpleJsonResourceReloadListen
       } catch (JsonSyntaxException e) {
         Constants.LOGGER.error("Error parsing {} dimension tweaks!", entry.getKey(), e);
       }
-    }
-  }
-
-  /**
-   * {@link ResourceKey} {@link JsonDeserializer}.
-   */
-  public static class ResourceKeyDeserializer implements JsonDeserializer<ResourceKey<Level>> {
-    @Override
-    public ResourceKey<Level> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-      return ResourceKey.create(Registries.DIMENSION, new ResourceLocation(json.getAsString()));
     }
   }
 }
